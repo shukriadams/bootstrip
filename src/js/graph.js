06302deea1c49275220395aa2e787f9dd831e709
Shukri Adams <shukri.adams@gmail.com>
2021-03-01 21:20:22 +0100

    brought back circle graphs

--- src/js/graph.js
+++ src/js/graph.js
@@ -4,13 +4,13 @@ function renderCircleGraph(el){
     let options = {
             percent:  el.getAttribute('data-percent') || 25,
             size: el.getAttribute('data-size') || 220,
-            lineWidth: el.getAttribute('data-line') || 15,
+            color: el.getAttribute('data-color') || '#fff',
+            bgcolor: el.getAttribute('data-bgcolor') || '#000',
+            lineWidth: el.getAttribute('data-line') || 14,
             rotate: el.getAttribute('data-rotate') || 0
         },
-        canvas = document.createElement('canvas'),
-        span = document.createElement('span')
+        canvas = document.createElement('canvas')
 
-    span.textContent = options.percent + '%'
 
     if (typeof(G_vmlCanvasManager) !== 'undefined') 
         G_vmlCanvasManager.initElement(canvas)
@@ -18,7 +18,6 @@ function renderCircleGraph(el){
     let ctx = canvas.getContext('2d')
     canvas.width = canvas.height = options.size
 
-    el.appendChild(span)
     el.appendChild(canvas)
 
     ctx.translate(options.size / 2, options.size / 2) // change center
@@ -35,9 +34,10 @@ function renderCircleGraph(el){
                 ctx.stroke()
         }
 
-    drawCircle('#2B3038', options.lineWidth, 100 / 100)
-    drawCircle('#FF662B', options.lineWidth, options.percent / 100)
+    drawCircle(options.bgcolor, options.lineWidth, 100 / 100)
+    // making inner line 2 px thinner makes compensates for jaggies
+    drawCircle(options.color, options.lineWidth -2, options.percent / 100)
 }
 
-for (const element of document.querySelectorAll('.dashiGraph-circle'))
+for (const element of document.querySelectorAll('.circleGraph'))
     renderCircleGraph(element) 
