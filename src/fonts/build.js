0ebcf307c68e647e89adcfd6c6139c3903a4809e
Shukri Adams <shukri.adams@gmail.com>
2021-03-01 19:15:01 +0100

    tweaked side menu, more icons

--- src/fonts/build.js
+++ src/fonts/build.js
@@ -6,7 +6,17 @@ const webfontsGenerator = require('webfonts-generator')
  
 webfontsGenerator({
         files: [
-            'svgs/add.svg'
+            'svgs/add.svg',
+            'svgs/back-arrow.svg',
+            'svgs/check-symbol.svg',
+            'svgs/close-button.svg',
+            'svgs/favorite-heart-button.svg',
+            'svgs/keyboard-right-arrow-button.svg',
+            'svgs/menu-button.svg',
+            'svgs/rate-star-button.svg',
+            'svgs/searching-magnifying-glass.svg',
+            'svgs/shopping-cart.svg',
+            'svgs/tick-inside-circle.svg'
         ],
         dest: './../web/css/icons/',
         cssFontsUrl : '/css/icons',
