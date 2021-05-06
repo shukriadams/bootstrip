6ac2ace0e8e0eba3ca22245ee5af51f4eaa29e19
Shukri Adams <shukri.adams@gmail.com>
2021-05-06 13:10:42 +0200

    fixed path

--- src/build/build.sh
+++ src/build/build.sh
@@ -4,8 +4,10 @@ set -e
 # write current tag+commit hash to data file so this can be embedded into headers
 sh ./writeHash.sh
 
-# build the stuff
-docker run -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip/src && yarn && npm run build"
+cd ..
+
+# bound src folder in build container and build the stuff
+docker run -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip && yarn && npm run build"
 
 # upload content to static webhost
 docker run -e $HOST -e $USERNAME -e $PASSWORD -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip/src/web && rsync --recursive --ignore-times --times --compress --delete --progress --rsh="\""sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}"\"" ./*  ${USERNAME}@${HOST}:/srv/bootstrip/"
