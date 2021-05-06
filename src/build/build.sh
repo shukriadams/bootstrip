1e292949b5fcaeb9a571ed181be32e6dc257864b
Shukri Adams <shukri.adams@gmail.com>
2021-05-06 14:15:02 +0200

    fixed purge key

--- src/build/build.sh
+++ src/build/build.sh
@@ -7,16 +7,24 @@ sh ./writeHash.sh
 cd ..
 
 # bound src folder in build container and build the stuff
-docker run -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip && yarn && npm run build"
+docker run \
+    -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 \
+    sh -c "cd /tmp/bootstrip && yarn --no-bin-links && npm run build"
 
 # upload content to static webhost
-docker run -e $HOST -e $USERNAME -e $PASSWORD -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip/web && rsync --recursive --ignore-times --times --compress --delete --progress --rsh="\""sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}"\"" ./*  ${USERNAME}@${HOST}:/srv/bootstrip/"
+docker run \
+    -e $HOST \
+    -e $USERNAME \
+    -e $PASSWORD \
+    -v $(pwd):/tmp/bootstrip shukriadams/node12build:0.0.2 sh -c "cd /tmp/bootstrip/web && rsync --recursive --ignore-times --times --compress --delete --progress --rsh="\""sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}"\"" ./*  ${USERNAME}@${HOST}:/srv/bootstrip/"
 
 # upload release artefacts to github
 
 # purge cache on cloudlare
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_APP}/purge_cache" \
+curl \
+    -X POST  \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
     -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
     -H "Content-Type: application/json" \
-    --data '{"purge_everything":true}'
+    --data '{"purge_everything":true}' \
+    "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache"
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update
 
+# docker
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo usermod -aG docker vagrant
+
 sudo apt-get install git -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
